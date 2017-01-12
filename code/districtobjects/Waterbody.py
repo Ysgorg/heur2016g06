48602d46ae8d833ac96635d77b2d9172aa6041b3
Ysgorg <yysgorg@gmail.com>
2017-01-12 18:37:46 +0100

    - added getType to Waterbody.py for convenience

--- code/districtobjects/Waterbody.py
+++ code/districtobjects/Waterbody.py
@@ -4,3 +4,6 @@ from Placeable import Placeable
 class Waterbody(Placeable):
     def __init__(self, x, y, width, height):
         super(Waterbody, self).__init__(x, y, width, height)
+
+    def getType(self):
+        return "Water"

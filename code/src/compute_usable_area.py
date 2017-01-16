36bfb9ff9c69cf7fe902416998a3c37422305259
Ysgorg <yysgorg@gmail.com>
2017-01-16 15:05:50 +0100

    debug + visualize

--- code/src/compute_usable_area.py
+++ code/src/compute_usable_area.py
@@ -2,42 +2,50 @@ from districtobjects.FamilyHome import FamilyHome
 from districtobjects.Mansion import Mansion
 from districtobjects.Bungalow import Bungalow
 
-def compute_usable_area(plan):
+def compute_usable_area(plan,visualize=True):
 
     # returns the proportion of area of a plan which can potentially be covered by a residence
 
-    bm = [[0 for x in range(plan.WIDTH)] for y in range(plan.HEIGHT)]
+    bm = [[0 for y in range(plan.HEIGHT)] for x in range(plan.WIDTH)]
 
     for x in range(0,plan.WIDTH):
         for y in range(0,plan.HEIGHT):
             m = Mansion(x,y)
             b = Bungalow(x,y)
             h = FamilyHome(x,y)
-            #print "correct?:",plan.correctlyPlaced(h)
-            if plan.correctlyPlaced(h):
 
-                try:
-                    bm[x][y] = 1
-                    bm[min(int(x+h.getWidth()),plan.WIDTH-2)][min(int(y+h.getHeight()),plan.HEIGHT-2)] = 1
-                except Exception:pass
+            if plan.correctlyPlaced(h):
+                bm[x][y] = 1
+                bm[min(int(x+h.getWidth()),plan.WIDTH-1)][min(int(y+h.getHeight()),plan.HEIGHT-1)] = 1
+
             if plan.correctlyPlaced(m):
-                try:
-                    bm[x][y] = 1
-                    bm[min(int(x+m.getWidth()),plan.WIDTH-2)][min(int(y+h.getHeight()),plan.HEIGHT-2)] = 1
-                except Exception:
-                    pass
+                bm[x][y] = 1
+                bm[min(int(x+m.getWidth()),plan.WIDTH-1)][min(int(y+m.getHeight()),plan.HEIGHT-1)] = 1
+
             if plan.correctlyPlaced(b):
-                try:
-                    bm[x][y] = 1
-                    bm[min(int(x+b.getWidth()),plan.WIDTH-2)][min(int(y+b.getHeight()),plan.HEIGHT-2)] = 1
-                except Exception:pass
+                bm[x][y] = 1
+                bm[min(int(x+b.getWidth()),plan.WIDTH-1)][min(int(y+b.getHeight()),plan.HEIGHT-1)] = 1
+
     count = 0
     tot = plan.WIDTH*plan.HEIGHT
-    for x in range(0,plan.WIDTH):
-        for y in range(0,plan.HEIGHT):
-            try:
-                count+=bm[x][y]
-            except Exception:
-                tot-=1
+    for x in range(plan.WIDTH):
+        for y in range(plan.HEIGHT):
+            count+=bm[x][y]
 
-    return float(count)/tot
+    useful_proportion = float(count)/tot
+    print useful_proportion
+
+    if visualize:
+
+        from GroundplanFrame import GroundplanFrame
+        frame = GroundplanFrame(plan)
+        frame.repaint(plan)
+
+        tot = plan.WIDTH*plan.HEIGHT
+        for x in range(plan.WIDTH):
+            for y in range(plan.HEIGHT):
+                if bm[x][y]:frame.mark(x,y,'black')
+
+        frame.updateit()
+        
+    return useful_proportion

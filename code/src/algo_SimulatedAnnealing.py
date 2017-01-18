5aad3af93b36ba54f72bd8e57a10531745fdee63
Marvin <ross@rchadwick.co.uk>
2017-01-18 20:40:47 +0100

    removed unnecessary repeat function calls

--- code/src/algo_SimulatedAnnealing.py
+++ code/src/algo_SimulatedAnnealing.py
@@ -3,19 +3,19 @@ from random import random
 from src.GroundplanFrame import GroundplanFrame
 
 
-def simulated_annealing(init_state, max_iterations, generateNeighbor):
+def simulated_annealing(init_state, max_iterations, generateNeighborFunc):
     state = init_state.deepCopy()
-    best_state = init_state.deepCopy()
+    best_state = state
 
     frame = GroundplanFrame(state)
-    bframe = GroundplanFrame(best_state)
+    bframe = frame
 
     for i in range(max_iterations):
 
         frame.repaint(state)
         bframe.repaint(best_state)
 
-        neighbor = generateNeighbor(state.deepCopy())
+        neighbor = generateNeighborFunc(state.deepCopy())
         temperature = float(i + 1) / max_iterations
 
         print state.getPlanValue(), neighbor.getPlanValue(), best_state.getPlanValue()

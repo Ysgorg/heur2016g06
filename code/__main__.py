eb29192d138de4cdd2214a5643a4bce2d10423a9
Marvin <ross@rchadwick.co.uk>
2017-01-23 14:48:36 +0100

    Re-enabled clustering now that the missing class is available

--- code/__main__.py
+++ code/__main__.py
@@ -16,7 +16,7 @@ from src.neighbor_random import neighbor_random
 # residence placers
 from src.validstate_rndm import validstate_rndm
 from src.validstate_tight import validstate_tight
-#from src.validstate_cluster import validstate_cluster
+from src.validstate_cluster import validstate_cluster
 
 # water and playground placers
 from src.base_dynamic import base_dynamic

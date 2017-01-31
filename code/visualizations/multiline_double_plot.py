cfe6d6b8cb09c3ea26cef83e6a4f5ca96531830e
TotalKnob <TotalKnob@users.noreply.github.com>
2017-01-31 15:24:29 +0100

    Fixed formatting warnings

--- code/visualizations/multiline_double_plot.py
+++ code/visualizations/multiline_double_plot.py
@@ -1,7 +1,8 @@
 
-X=0
-Y1=1
-Y2=2
+X = 0
+Y1 = 1
+Y2 = 2
+
 
 def multiline_double_plot(
         dataseries,
@@ -12,25 +13,25 @@ def multiline_double_plot(
 
     # assert that the input is as expected
 
-    assert isinstance(x_name,str)
-    assert isinstance(y1_name,str)
-    assert isinstance(y2_name,str)
-    assert isinstance(main_label,str)
+    assert isinstance(x_name, str)
+    assert isinstance(y1_name, str)
+    assert isinstance(y2_name, str)
+    assert isinstance(main_label, str)
 
-    assert isinstance(dataseries,list)
+    assert isinstance(dataseries, list)
     assert len(dataseries) > 0
 
     for series in dataseries:
-        assert isinstance(series,list)
-        assert len(series)==2
+        assert isinstance(series, list)
+        assert len(series) == 2
 
         # metadata can optionally be put somewhere on the visualization
         meta = series[0]
-        assert 'pg' in meta # enable playground
-        assert 'nh' in meta # number of houses
-        assert 'base' in meta # which base to use
-        assert 'algo' in meta # algorithm (zoom/sa/hillclimber)
-        assert 'tf' in meta or 'nc' in meta # tight fit algo or number of candidates (hill climber)
+        assert 'pg' in meta  # enable playground
+        assert 'nh' in meta  # number of houses
+        assert 'base' in meta  # which base to use
+        assert 'algo' in meta  # algorithm (zoom/sa/hillclimber)
+        assert 'tf' in meta or 'nc' in meta  # tight fit algo or number of candidates (hill climber)
         assert meta[meta['variant']] == '**variant**'
 
         datapoints = series[1]
@@ -38,31 +39,30 @@ def multiline_double_plot(
         for d in datapoints:
             assert isinstance(d, list)
             assert len(d) == 3
-            assert isinstance(d[X],float) or isinstance(d[X],int)
-            assert isinstance(d[Y1],float) or d[Y1] == 0
-            assert isinstance(d[Y2],float)
+            assert isinstance(d[X], float) or isinstance(d[X], int)
+            assert isinstance(d[Y1], float) or d[Y1] == 0
+            assert isinstance(d[Y2], float)
 
-    # plot the values
+            # plot the values
 
 
-
-def test_mdp(): # test multiline double plot
+def test_mdp():  # test multiline double plot
 
     multiline_double_plot([
-        [ # this is a separate data series. it should have it's own color in the output visualization
+        [  # this is a separate data series. it should have it's own color in the output visualization
             {   # this part is meta data that can optionally be put on the plot in some way
-                'pg':True,
-                'nh':'**variant**',
-                'base':'base_dynamic',
-                'algo':'Zoom',
-                'tf':'TightFit_WB',
-                'variant':'nh'
+                'pg': True,
+                'nh': '**variant**',
+                'base': 'base_dynamic',
+                'algo': 'Zoom',
+                'tf': 'TightFit_WB',
+                'variant': 'nh'
             },
             [
                 [
-                    50, # x
-                    200.0, # y1
-                    210.0 # y2
+                    50,  # x
+                    200.0,  # y1
+                    210.0  # y2
                 ],
                 [
                     60,
@@ -71,20 +71,20 @@ def test_mdp(): # test multiline double plot
                 ]
             ]
         ],
-        [ # another separate dataset
+        [  # another separate dataset
             {
-                'pg':False,
-                'nh':'**variant**',
-                'base':'base_a',
-                'algo':'SimmulatedAnnealing_2',
-                'tf':'TightFit_WB',
-                'variant':'nh'
+                'pg': False,
+                'nh': '**variant**',
+                'base': 'base_a',
+                'algo': 'SimmulatedAnnealing_2',
+                'tf': 'TightFit_WB',
+                'variant': 'nh'
             },
             [
                 [
-                    50, # x
-                    160.6, # y1
-                    180.0 # y2
+                    50,  # x
+                    160.6,  # y1
+                    180.0  # y2
                 ],
                 [
                     60,
